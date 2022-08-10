bdb8d32b5d5f2208bfbaf6f6d0e31de16752259a
moksu27 <qotnahr@naver.com>
2022-08-10 16:17:29 +0900

    sharpening practice

--- sharpening1.py
+++ sharpening1.py
@@ -7,9 +7,10 @@ src = cv2.imread('rose.bmp', cv2.IMREAD_GRAYSCALE)
 if src is None:
     print('Image load failed!')
     sys.exit()
+blr = cv2.GaussianBlur(src, (0,0), 2)
+#dst= cv2.addWeighted(src,2, blr,-1,0)
+dst = np.clip(2.*src-blr,0,255).astype(np.uint8)
 
-blr = cv2.GaussianBlur(src, (0, 0), 2)
-dst = np.clip(2.0*src - blr, 0, 255).astype(np.uint8)
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
